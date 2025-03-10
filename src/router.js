7e5cf08e4cc61b4830b7ebb89e005d8e444e1057
menobom7 <geaga7e7@gmail.com>
2025-03-10 20:07:24 -0300

    configurando vue-router

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '@/components/Login.vue';
+
+Vue.use(Router);
+
+export default new Router({
+    mode: "history",
+    routes: [
+        {path: "/login", component: Login}
+    ]
+})
